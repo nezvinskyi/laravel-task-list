95441729f7b6f8abb9bb47297a8874abb64f1587
Dmitry Nezvinskyi <nezvinskyi@gmail.com>
2023-11-23 13:53:18 +0100

    Styling Flash Message & Apline.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel 10 Task List</title>
     @yield('styles')
-  <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="//unpkg.com/alpinejs" defer></script>
     {{-- blade-formatter-disable --}}
     <style type="text/tailwindcss">
         .btn{
@@ -30,21 +32,41 @@
     </style>
     {{-- blade-formatter-enable --}}
 
-    
+
     <style>
-        .success-message{
+        .success-message {
             color: green;
             font-size: .85rem;
         }
     </style>
 </head>
-<body class="container mx-auto mt-10 mb-10 max-w-lg bg-yellow-50 dark:bg-slate-700">
-    <h1 class="mb-4 text-2xl text-gray-700 dark:text-yellow-50">@yield('title')</h1>
 
-    @if(session()->has('success'))
-        <div class="success-message">{{ session('success') }}</div>
-    @endif
-    
+<body
+    class="container mx-auto mt-10 mb-10 max-w-lg bg-yellow-50 dark:bg-slate-700">
+    <h1 class="mb-4 text-2xl text-gray-700 dark:text-yellow-50">@yield('title')
+    </h1>
+    <div class="" x-data="{ flash: true }">
+        @if(session()->has('success'))
+        <div x-show="flash" class="relative mb-10 rounded border border-green-700 bg-green-100 dark:bg-green-700 px-4 py-3 text-lg text-green-700 dark:text-green-200"
+            role="alert">
+
+            <strong class="font-bold">Success!</strong>
+            <div class="">{{ session('success') }}</div>
+            <span @click="flash = false" class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor"
+                    class="h-6 w-6 cursor-pointer">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </span>
+        </div>
+        @endif
+
+    </div>
+
     <div class="">@yield('content')</div>
 </body>
+
 </html>
